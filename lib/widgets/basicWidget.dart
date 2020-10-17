f58f2df677f82c1c5016320b32f1a65fa43597ab
SOJAN KS <ks4sojan@gmail.com>
2020-10-17 12:47:31 +0530

    MediaQuery and blur offset etc

--- lib/widgets/basicWidget.dart
+++ lib/widgets/basicWidget.dart
@@ -4,8 +4,8 @@ class BasicWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width * .6,
+      height: 400,
       margin: EdgeInsets.all(0),
       padding: EdgeInsets.all(40),
       decoration: BoxDecoration(
@@ -14,12 +14,13 @@ class BasicWidget extends StatelessWidget {
             color: Colors.black,
             width: 2,
           ),
-          borderRadius: BorderRadius.circular(0),
+          borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
+              color: Colors.grey,
               blurRadius: 10,
-              offset: Offset(10, 10),
               spreadRadius: 10,
+              offset: Offset(10, 10),
             ),
           ]),
       child: Center(
